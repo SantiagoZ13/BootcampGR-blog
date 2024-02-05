8667a34fbce353d7ea46fdd4e2232dc3e8e33e4b
SantiagoZ13 <diazsantiagoponce1@gmail.com>
2024-02-04 19:13:30 -0500

    Add date humanized in the blog

---
+++ src/scripts/main.js
@@ -0,0 +1,16 @@
+import humanize from "./utils/humanize.js"
+
+function capitalize(str){
+    return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+function humanizeDate ($date, i){
+    const datetime = $date.getAttribute("datetime")
+    const humanizedDate = humanize(datetime) 
+    console.log(humanizedDate, i)
+    $date.textContent = capitalize(humanizedDate)
+}
+
+const $dates = document.querySelectorAll("time")
+
+$dates.forEach(humanizeDate)
